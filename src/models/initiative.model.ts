355a7d8e8deda02bd39057552fdd5fd9d07826bc
MHD <mhd.tahawi@gmail.com>
2020-04-04 16:19:25 +0200

    Add Initiative model

---
+++ src/models/initiative.model.ts
@@ -0,0 +1,36 @@
+import {
+  AllowNull,
+  AutoIncrement,
+  BelongsTo,
+  Column,
+  DataType,
+  ForeignKey,
+  Model,
+  PrimaryKey,
+  Table
+} from 'sequelize-typescript';
+import User from "./users.model";
+
+@Table({ modelName: 'initiative', timestamps: true, paranoid: false })
+export default class Initiative extends Model<Initiative> {
+  @PrimaryKey
+  @AutoIncrement
+  @Column
+  id: number;
+
+  @AllowNull(false)
+  @Column(DataType.STRING(45))
+  category: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING(255))
+  description: string;
+
+  @AllowNull(false)
+  @ForeignKey(() => User)
+  @Column
+  userId: number;
+
+  @BelongsTo(() => User)
+  user: User;
+}
