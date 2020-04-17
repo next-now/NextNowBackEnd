136032755e531a6981d5bc7a91b52bcb994d891a
MHD <mhd.tahawi@gmail.com>
2020-04-17 23:27:34 +0200

    Update blockchain.service.ts: use dummy implementation for now

--- src/services/blockchain.service.ts
+++ src/services/blockchain.service.ts
@@ -8,52 +8,16 @@ class BlockchainService {
         this.api = axios.create()
     }
 
-    private getHeaders(): any {
-        return {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${process.env.BLOCKCHAIN_SERVICE_TOKEN}`,
-            'Grpc-metadata-space': "7a24f929-dd96-49c4-a306-3d16269ad920"
-        }
-    }
-
     public async createWallet(): Promise<string> {
-        const path: string = `${process.env.BLOCKCHAIN_SERVICE_BASE_PATH}/functions-invoke/create`;
-        const payload = {
-            "walletUUID": uuidv4(),
-        };
-        const reply = await this.api.post(path, payload, {headers: this.getHeaders()})
-        return reply.data.result.uuid;
+        return uuidv4();
     }
 
     public async getWalletBalance(walletId: string): Promise<number> {
-        const path: string = `${process.env.BLOCKCHAIN_SERVICE_BASE_PATH}/functions-invoke/query`;
-        const payload = {
-            walletUUID: walletId
-        };
-        const reply = await this.api.post(path, payload, {headers: this.getHeaders()})
-        return reply.data.result.balance;
+        return 0;
     }
 
     public async sendTokens(target: string, amount: number): Promise<string> {
-        const path: string = `${process.env.BLOCKCHAIN_SERVICE_BASE_PATH}/functions-invoke/transfer`;
-        const payload = {
-            walletUUID: target,
-            amount: amount
-        };
-
-        const reply = await this.api.post(path, payload, {headers: this.getHeaders()})
         return uuidv4(); // no transaction
-    }
-
-    public async init(masterBalance: number): Promise<void> {
-        const path: string = `${process.env.BLOCKCHAIN_SERVICE_BASE_PATH}/functions-invoke/initialize`;
-        const payload = {
-            masterBalance: masterBalance,
-        };
-
-        const reply = await this.api.post(path, payload, {headers: this.getHeaders()});
-        console.log(reply);
-
     }
 }
 export default BlockchainService;
